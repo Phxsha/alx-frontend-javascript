dde385f069da9efe99569ea53185122f6e857624
Phxsha <phashateboho@gmail.com>
2024-07-30 17:27:37 +0000

    task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+  // Create a new Map with predefined grocery items and quantities
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+
+  return groceries;
+}
