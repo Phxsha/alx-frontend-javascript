f7b7958383d7b5da7b3d2ddc83a74ade3d9037c2
Phxsha <phashateboho@gmail.com>
2024-07-30 17:42:12 +0000

    task 10, and 8 fix

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+export default function updateUniqueItems(map) {
+  // Check if the argument is a Map
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  // Iterate over the entries of the map
+  for (const [key, value] of map.entries()) {
+    // If the quantity is 1, update it to 100
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+
+  return map;
+}
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
-export default function cleanSet(set, startString) {
-  // Filter the set to include only values that start with startString
-  // Map over these values to remove startString from the beginning of each
-  // Join the resulting array into a string with '-' as the separator
-  return Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
+export default function cleanSet(xSet, xStartString) {
+  if (!xSet || !xStartString || !(xSet instanceof Set) || typeof xStartString !== 'string') {
+    return '';
+  }
+  return Array.from(xSet)
+    .filter((ele) => ele && ele.startsWith(xStartString))
+    .map((ele) => ele.replace(xStartString, ''))
     .join('-');
 }
